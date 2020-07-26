f431632cab29bfd6ed1d7750a9c65371ed9a8b97
kaique.motta@hotmail.com <xrlxrl157>
2020-07-26 19:04:55 -0300

    first commit

---
+++ src/main/java/kafka/servico/GeradorVendas.java
@@ -0,0 +1,41 @@
+package kafka.servico;
+
+import java.math.BigDecimal;
+import java.util.Properties;
+import java.util.Random;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import kafka.model.Venda;
+import kafka.serializer.VendaSerializer;
+
+public class GeradorVendas {
+
+	private static Random rand = new Random();
+	private static long operacao = 0;
+	private static BigDecimal valorIngreco = BigDecimal.valueOf(500);
+
+	public static void main(String[] args) throws InterruptedException {
+		Properties properties = new Properties();
+		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, VendaSerializer.class.getName());
+		try(KafkaProducer<String, Venda> producer = new KafkaProducer<String, Venda>(properties)){
+			while (true) {
+				Venda venda = geraVenda();
+				ProducerRecord<String, Venda> record = new ProducerRecord<String, Venda>("venda-ingressos", venda);
+				producer.send(record);
+				Thread.sleep(200); 
+			}	
+		}
+	}
+
+	private static Venda geraVenda() {
+		long cliente = rand.nextLong();
+		int qtdIngressos = rand.nextInt();
+		return new Venda(operacao++, cliente, qtdIngressos, valorIngreco.multiply(BigDecimal.valueOf(qtdIngressos)));
+	}
+}
